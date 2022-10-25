ebd2cc489aa3c3326ced87f9268fb93fb6ce48de
Caameron <caamnaka@gmail.com>
2022-10-24 20:48:44 -0700

    5 and 6 done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,24 @@
 const sumAll = function(num1, num2) {
+    if(typeof(num1) !== 'number' || typeof(num2) !== 'number'){
+        return "ERROR";
+    }
+    if(num1 < 0 || num2 < 0){
+        return "ERROR";
+    }
+
+    let biggerNum = 0;
+    let smallerNum = 0;
+    if(num1 > num2){
+        biggerNum = num1;
+        smallerNum = num2;
+    }
+    else {
+        biggerNum = num2;
+        smallerNum = num1;
+    } 
+    
     let returnNum = 0;
-    for(let num = num1; num <= num2; num++)
+    for(let num = smallerNum; num <= biggerNum; num++)
     {
         returnNum = returnNum + num;
     }
